7d0cda106c11987b2747f50602e413da7fd8db4a
NtoStudy <2943343011@qq.com>
2025-04-05 17:47:40 +0800

    :heavy_plus_sign: feat: 新增tailwindcss的依赖

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src'), // 配置 @ 指向 src 目录
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
 })
